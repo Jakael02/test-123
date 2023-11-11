5e90d15a5fac9ecd41e676612fc0ff2f68b7a61b
Jakael02 <148093654+Jakael02@users.noreply.github.com>
2023-11-11 12:53:11 -0500

    Create 4_Test.py

---
+++ pages/4_Test.py
@@ -0,0 +1,14 @@
+import streamlit 
+
+#Title must come pre-defined font size & type
+streamlit.title('My Parents New Healthy Diner')
+
+#Header
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
