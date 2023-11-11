2b4b2057ff62ada89b4e5ccd6d9296fcf59fd55f
Jakael02 <148093654+Jakael02@users.noreply.github.com>
2023-11-11 12:39:29 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,18 +1,27 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+#LOGGER = get_logger(__name__)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+#def run():
+#   st.set_page_config(
+#       page_title="Hello",
+#       page_icon="👋",
+#   )
 
     st.write("# Welcome to Streamlit 2.0!! 👋")
 
     st.sidebar.success("Select a demo above.")
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 if __name__ == "__main__":
     run()
