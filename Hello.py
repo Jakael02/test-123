0ca84d246d73484729505cd5a5a914716df5aedf
Jakael02 <148093654+Jakael02@users.noreply.github.com>
2023-11-12 07:19:35 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,3 +12,12 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
